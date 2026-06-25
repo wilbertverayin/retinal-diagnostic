d3c9cb62a6fccb844e904b807edecb3cccac6064
Wilbert Verayin <wilbert@dataspark.nl>
2026-06-25 21:27:24 +0800

    chore: configure custom domain retinal-diagnostic.visibleclinic.com

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: process.env.GITHUB_PAGES === 'true' ? '/retinal-diagnostic' : '',
   typescript: {
     ignoreBuildErrors: true,
   },
